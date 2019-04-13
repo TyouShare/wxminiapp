00f246cc66b97b150d89fb8730b15b1d3d7fe1c3
zhangbinbin <zhangbinbin@xiaoniu.com>
2019-04-13 15:01:33 +0800

    配置从github上取

--- pages/me/me.js
+++ pages/me/me.js
@@ -5,18 +5,66 @@ Page({
    * 页面的初始数据
    */
   data: {
-    name:"xxx",
-    mobile:"12345678",
+    name:"",
+    mobile:"",
     descr:"自我介绍",
-    avatar:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1555066398046&di=5c8a825f999787ed36717ca948207441&imgtype=0&src=http%3A%2F%2Fimg.mp.itc.cn%2Fupload%2F20161221%2F9760e3d5c61f41009cdddfb31795b8af_th.jpeg"
+    avatar:"",
+    hasData:false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 页面加载
+    var that = this;
+    wx.getStorage({
+      key: 'me',
+      success: function (res) {
+        var meData = JSON.parse(unescape(res.data))
+        that.setData({
+          name: meData.name,
+          descr: meData.descr,
+          mobile: meData.mobile,
+          avatar: meData.avatar,
+          hasData: true
+        })
+      },
+    })
 
+    this.loadConfig();
   },
+
+  loadConfig: function () {
+    var that = this;
+    wx.request({
+      url: "https://raw.githubusercontent.com/TyouShare/mallConfig/master/me.json",
+      success(result) {
+        var meData = result.data;
+        if (meData) {
+          if (that.data.hasData == false) {
+            that.setData({
+              name: meData.name,
+              descr: meData.descr,
+              mobile: meData.mobile,
+              avatar: meData.avatar,
+              hasData: true
+            })
+          }
+          // 保存起来，下次用
+          var encodeData = escape(JSON.stringify(result.data))
+          wx.setStorage({
+            key: 'me',
+            data: encodeData,
+          })
+        }
+      },
+      fail(result) {
+        console.log(result)
+      }
+    })
+  },
+
   avatat_tap: function (arg){
     console.log(arg);
     // 预览头像
--- pages/shopList/shopList.js
+++ pages/shopList/shopList.js
@@ -1,40 +1,49 @@
 // 物品列表页面
 Page({
   data: {
-    item: [{
-        "name": "面膜",
-        "descr": "这款面膜很好",
-        "price": "人民币 150元",
-        "imageUrl": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1555066192050&di=538e6eb3e2e881fecbdb63bc0361f949&imgtype=0&src=http%3A%2F%2Fimg007.hc360.cn%2Fm8%2FM07%2FD5%2F17%2FwKhQplb12vSEc7W7AAAAALr2l3k971.jpg",
-        "imageList":[
-          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1555066192050&di=538e6eb3e2e881fecbdb63bc0361f949&imgtype=0&src=http%3A%2F%2Fimg007.hc360.cn%2Fm8%2FM07%2FD5%2F17%2FwKhQplb12vSEc7W7AAAAALr2l3k971.jpg",
-          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1555066192050&di=538e6eb3e2e881fecbdb63bc0361f949&imgtype=0&src=http%3A%2F%2Fimg007.hc360.cn%2Fm8%2FM07%2FD5%2F17%2FwKhQplb12vSEc7W7AAAAALr2l3k971.jpg",
-          "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1555066192050&di=538e6eb3e2e881fecbdb63bc0361f949&imgtype=0&src=http%3A%2F%2Fimg007.hc360.cn%2Fm8%2FM07%2FD5%2F17%2FwKhQplb12vSEc7W7AAAAALr2l3k971.jpg"
-        ]
-      }, {
-        "mobile" : "123456",
-        "name": "卸妆棉",
-        "descr": "这款面膜很好",
-        "price": "人民币 150元",
-        "imageUrl": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1555066398046&di=5c8a825f999787ed36717ca948207441&imgtype=0&src=http%3A%2F%2Fimg.mp.itc.cn%2Fupload%2F20161221%2F9760e3d5c61f41009cdddfb31795b8af_th.jpeg"
-      }, {
-        "name": "面膜",
-        "descr": "这款面膜很好",
-        "price": "人民币 150元",
-        "imageUrl": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1555066192050&di=538e6eb3e2e881fecbdb63bc0361f949&imgtype=0&src=http%3A%2F%2Fimg007.hc360.cn%2Fm8%2FM07%2FD5%2F17%2FwKhQplb12vSEc7W7AAAAALr2l3k971.jpg"
-      }, {
-        "name": "卸妆棉",
-        "descr": "这款面膜很好",
-        "price": "人民币 150元",
-        "imageUrl": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1555066398046&di=5c8a825f999787ed36717ca948207441&imgtype=0&src=http%3A%2F%2Fimg.mp.itc.cn%2Fupload%2F20161221%2F9760e3d5c61f41009cdddfb31795b8af_th.jpeg"
-      }
-
-    ],
-    text: "商品展示列表"
+    item: []
   },
 
-  onLoad:function(options) {
+  onLoad: function(options) {
     // 页面加载
+    var that = this;
+    wx.getStorage({
+      key: 'list',
+      success: function(res) {
+        var listData = JSON.parse(unescape(res.data))
+        that.setData({
+          item: listData
+        })
+      },
+    })
+
+    this.loadConfig();
+  },
+
+  loadConfig: function() {
+    var that = this;
+    wx.request({
+      url: "https://raw.githubusercontent.com/TyouShare/mallConfig/master/list_config.json",
+      success(result) {
+        if (result.data) {
+          if (that.data.item.length == 0){
+            console.log(result.data);
+            that.setData({
+              item: result.data
+            })
+          }
+          // 保存起来，下次用
+          var encodeData = escape(JSON.stringify(result.data))
+          wx.setStorage({
+            key: 'list',
+            data: encodeData,
+          })
+        }
+      },
+      fail(result) {
+        console.log(result)
+      }
+    })
   },
 
   // cell 点击事件
@@ -46,7 +55,7 @@ Page({
     })
   },
 
-  mobile_tap: function (arg) {
+  mobile_tap: function(arg) {
     //
     var mobile = arg.currentTarget.dataset.mobile;
     const connect = require('../../utils/connect.js')
